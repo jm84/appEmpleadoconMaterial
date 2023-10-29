81a33e4a2d2b1c7b7e5d3c2b1395f2ae5dd54956
jm84 <jignamolina@gmail.com>
2023-10-29 11:09:02 -0300

    app empleado hasta la creacion del servicio

---
+++ src/app/components/shared/angular-material/angular-material.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// angular material
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+  ],
+  exports: [
+    MatSliderModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+  ],
+})
+export class AngularMaterialModule {}
---
+++ src/app/models/empleado.ts
@@ -0,0 +1,10 @@
+export class Empleado {
+  nombreCompleto: string = '';
+  telefono: number = 0;
+  correo: string = '';
+  fechaIngreso: Date = new Date('29/10/23');
+  estadoCivil: string = '';
+  sexo: string = '';
+
+  constructor() {}
+}
